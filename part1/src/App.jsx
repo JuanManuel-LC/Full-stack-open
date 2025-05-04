f2a560acae48c42759c0e7ac9197e92b92afabcb
juan-code <juanlaboral04@gmail.com>
2025-05-03 23:21:42 -0500

    exercises 1.5

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,17 +1,36 @@
 function App() {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of component'
-  const exercises3 = 14
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of component',
+        exercises: 14
+      }
+    ]
+  }
+
+  // const part1 = 'Fundamentals of React'
+  // const exercises1 = 10
+
+  // const part2 = 'Using props to pass data'
+  // const exercises2 = 7
+
+  // const part3 = 'State of component'
+  // const exercises3 = 14
 
   return (
     <>
-      <Header course={course} />
-      <Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
-      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
 
     </>
   )
@@ -29,21 +48,34 @@ function Header(props) {
 // Componente para el contenido
 function Content(props) {
 
+
   return (
     <>
-      <p>
-        {props.part1} {props.exercises1}
-      </p>
-      <p>
-        {props.part2} {props.exercises2}
-      </p>
-      <p>
-        {props.part3} {props.exercises3}
-      </p>
-
+      <Part parts={props.parts} />
     </>
   )
 
+}
+
+
+// Componente para las partes del componente Content
+const Part = (props) => {
+  console.log(props);
+
+
+  return (
+    <>
+      <p>
+        {props.parts[0].name} {props.parts[0].exercises}
+      </p>
+      <p>
+        {props.parts[1].name} {props.parts[1].exercises}
+      </p>
+      <p>
+        {props.parts[2].name} {props.parts[2].exercises}
+      </p>
+    </>
+  )
 }
 
 
@@ -51,7 +83,7 @@ function Content(props) {
 function Total(props) {
   return (
     <>
-      <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+      <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
     </>
   )
 
