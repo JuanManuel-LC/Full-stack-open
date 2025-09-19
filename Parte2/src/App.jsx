997a2c26d3486e4569b3fac5dc075ef82eeaf5bf
Juan Manuel LC <juanlaboral04@gmail.com>
2025-09-19 10:20:37 -0500

    Continuar practicando

---
+++ Parte2/src/App.jsx
@@ -0,0 +1,52 @@
+
+// ? Desestructuracion version 1
+// const Hello = (props) => {
+//   // Desestructuracion utilizando name y age
+//   const { name, age } = props
+//   const bornYear = () => new Date().getFullYear() - age
+
+//   return (
+//     <>
+//       <p>
+//         Hello {name}, you are {age} years old
+//       </p>
+//       <p>
+//         So you were probably born in {bornYear()}
+//       </p>
+//     </>
+//   )
+// }
+
+// ? Desestructuracion version 2
+const Hello = ({ name, age }) => {
+  // Desestructuracion utilizando name y age
+  const bornYear = () => new Date().getFullYear() - age
+
+  return (
+    <>
+      <p>
+        Hello {name}, you are {age} years old
+      </p>
+      <p>
+        So you were probably born in {bornYear()}
+      </p>
+    </>
+  )
+}
+
+
+const App = () => {
+  const name = 'Peter'
+  const age = 10
+  return (
+    <>
+      <h1>Greetings</h1>
+      <Hello name='Maya' age={26 + 10} />
+      <Hello name={name} age={age} />
+    </>
+  )
+
+
+}
+
+export default App
