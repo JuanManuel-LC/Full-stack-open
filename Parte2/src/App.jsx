fb25c23222bfcd4d3898239ba88864367796a8c7
JuanMLC-laptop <juanlaboral04@gmail.com>
2025-09-21 11:48:20 -0500

    No declara componentes dentro de otro componente

--- Parte2/src/App.jsx
+++ Parte2/src/App.jsx
@@ -1,83 +1,33 @@
-import { useState } from "react"
+import { useState } from "react";
 
-const History = (props) => {
-  if (props.allClicks.length === 0) {
-    return (
-      <>
-        the App is used by pressing the buttons
-      </>
-    )
-  }
-  return (
-    <>
-      button press history: {props.allClicks.join(' ')}
-    </>
+const Display = (props) => <>{props.value}</>;
 
-  )
-}
-
-const Button = (props) => {
-  console.log(props);
-
-  const { handleClick, text } = props
-
-  return (
-    < button onClick={handleClick} >
-      {text}
-    </ button>
-  )
-}
-
+const Button = (props) => (
+    <button onClick={props.handleClick}>{props.text}</button>
+);
 
 const App = () => {
+    const [value, setValue] = useState(10);
 
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
-  const [allClicks, setAll] = useState([])
+    const setToValue = (newValue) => {
+        console.log("value now", newValue);
+        setValue(newValue);
+    };
 
-  const handleLeftClick = () => {
-    setAll(allClicks.concat('L'))
-    setLeft(left + 1)
-  }
+    return (
+        <div>
+            <Display value={value} />
+            <button onClick={() => setToValue(1000)} text="thousand">
+                thousand
+            </button>
+            <button onClick={() => setToValue(0)} text="zero">
+                reset
+            </button>
+            <button onClick={() => setToValue(value + 1)} text="increment">
+                increment
+            </button>
+        </div>
+    );
+};
 
-  const handleRightClick = () => {
-    setAll(allClicks.concat('R'))
-    setRight(right + 1)
-  }
-
-  // ! Que no hacer con los useState y useEffect
-
-  // if ( age > 10 ) {
-  //   // esto no funciona!
-  //   const [foobar, setFoobar] = useState(null)
-  // }
-
-  // for ( let i = 0; i < age; i++ ) {
-  //   // esto tampoco está bien
-  //   const [rightWay, setRightWay] = useState(false)
-  // }
-
-  // const notGood = () => {
-  //   // y esto también es ilegal
-  //   const [x, setX] = useState(-1000)
-  // }
-
-
-
-  return (
-    <>
-      {left}
-      <Button handleClick={handleLeftClick} text='left'>
-        left
-      </Button>
-      <Button handleClick={handleRightClick} text='right'>
-        right
-      </Button>
-      {right}
-      <History allClicks={allClicks} />
-    </>
-  )
-
-}
-
-export default App
+export default App;
